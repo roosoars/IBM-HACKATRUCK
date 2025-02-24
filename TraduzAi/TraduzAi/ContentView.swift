0da44102272300e798920a280131bb9b63be5d18
Rodrigo <roosoars@icloud.com>
2025-02-24 14:18:13 -0300

    Add files via upload

---
+++ TraduzAi/TraduzAi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TraduzAi
+//
+//  Created by Rodrigo Soares on 24/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TraduzAi/TraduzAi/TraduzAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraduzAiApp.swift
+//  TraduzAi
+//
+//  Created by Rodrigo Soares on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TraduzAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
