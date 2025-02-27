5a6b7909f6bc3724b957bc244bf966096672cc23
Rodrigo <roosoars@icloud.com>
2025-02-27 10:11:24 -0300

    Initial Commit

---
+++ TraduzAi/Section/Home/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  TraduzAi
+//
+//  Created by Rodrigo Soares on 24/02/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Tela Principal")
+                .font(.largeTitle)
+                .padding()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
